d23e579e7acb70c692dc19bc8d70b5e28d599b99
Lucas Lecot <lucaslecot@MacBook-Pro-de-Lucas-3.local>
2017-06-03 17:02:23 +0200

    site e-commerce

---
+++ siteWeb/js/app.js
@@ -0,0 +1,24 @@
+var active = 0;
+
+var links = document.querySelectorAll('#product nav ul li a');
+var divs = document.querySelectorAll('.product');
+var title = document.querySelector(".serie");
+
+for (var i = 0; i < links.length; i++) {
+    addLinkToDiv(i);
+}
+
+function addLinkToDiv(index) {
+    links[index].addEventListener('click', function () {
+
+        links[active].classList.remove('active-links');
+        divs[active].classList.remove('active');
+
+        title.innerHTML = links[index].innerText;
+        links[index].classList.add('active-links');
+        divs[index].classList.add('active');
+
+        active = index;
+
+    });
+}
